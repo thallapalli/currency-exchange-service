de48a73702deb333107932fd854dd6f36909f929
karnakar <karnakar@ymail.com>
2019-12-31 02:12:19 -0500

    Currency Exchnage aded

--- src/main/java/kt/springboot/currency/exchange/service/controller/CurrencyExchangeServiceController.java
+++ src/main/java/kt/springboot/currency/exchange/service/controller/CurrencyExchangeServiceController.java
@@ -24,7 +24,7 @@ public class CurrencyExchangeServiceController {
 		
 		ExchangeValue exchangeValue =exchangeValueRepo.findByFromAndTo(from, to);
 		environment.getProperty("local.server.port");
-		exchangeValue.setPort(Integer.parseInt(environment.getProperty("local.server.port")));
+		exchangeValue.setPort(Integer.parseInt("1"));
 	    return exchangeValue;
 		
 	}
