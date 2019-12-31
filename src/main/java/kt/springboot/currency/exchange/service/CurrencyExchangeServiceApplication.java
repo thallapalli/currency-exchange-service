40a6ba55bef25b4217c40461bad2ad60c1203646
karnakar <karnakar@ymail.com>
2019-12-31 00:16:01 -0500

    'first commi

---
+++ src/main/java/kt/springboot/currency/exchange/service/CurrencyExchangeServiceApplication.java
@@ -0,0 +1,13 @@
+package kt.springboot.currency.exchange.service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CurrencyExchangeServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CurrencyExchangeServiceApplication.class, args);
+	}
+
+}
